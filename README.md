696fb329361244038df1ed9fa4421e53f407af7d
shikhars371 <shikhars371@gmail.com>
2020-09-17 18:05:03 +0530

    latest

---
+++ README.md
@@ -0,0 +1,79 @@
+# dwebstore-swarm-networking
+[![Build Status](https://travis-ci.com/andrewosh/dwebstore-swarm-networking.svg?branch=master)](https://travis-ci.com/andrewosh/dwebstore-swarm-networking)
+
+A dwebstore networking module that uses [dwebswarm](https://github.com/dwebswarm/network) to discovery peers. This module powers the networking portion of the [Hyperdrive daemon](https://github.com/andrewosh/ddrive-daemon).
+
+Calls to `seed` or `unseed` will not be persisted across restarts, so you'll need to use a separate database that maps discovery keys to network configurations. The Hyperdrive daemon uses [Level](https://github.com/level/level) for this.
+
+Since dwebstore has an all-to-all replication model (any shared cores between two peers will be automatically replicated), only one connection needs to be maintained per peer. If multiple connections are opened to a single peer as a result of that peer announcing many keys, then these connections will be automatically deduplicated by comparing NOISE keypairs.
+
+### Installation
+```
+npm i dwebstore-swarm-networking -g
+```
+
+### Usage
+```js
+const SwarmNetworker = require('dwebstore-swarm-networking')
+const DWebstore = require('dwebstore')
+const ram = require('random-access-memory')
+
+const store = new DWebstore(ram)
+await store.ready()
+
+const networker = new SwarmNetworker(store)
+
+// Start announcing or lookup up a discovery key on the DHT.
+await networker.join(discoveryKey, { announce: true, lookup: true })
+
+// Stop announcing or looking up a discovery key.
+networker.leave(discoveryKey)
+
+// Shut down the swarm (and unnanounce all keys)
+await networker.close()
+```
+
+### API
+
+#### `const networker = new SwarmNetworker(dwebstore, networkingOptions = {})`
+Creates a new SwarmNetworker that will open replication streams on the `dwebstore` instance argument.
+
+`networkOpts` is an options map that can include all [dwebswarm](https://github.com/dwebswarm/dwebswarm) options (which will be passed to the internal swarm instance) as well as:
+```js
+{
+  id: crypto.randomBytes(32), // A randomly-generated peer ID,
+  keyPair: HypercoreProtocol.keyPair(), // A NOISE keypair that's used across all connections.
+}
+```
+
+#### `await networker.join(discoveryKey, opts = {})`
+Join the swarm with the `discoveryKey` argument as the topic.
+
+If this is the first time a `join` or `leave` has been called, the swarm instance will be created automatically.
+
+Waits for the topic to be fully joined before resolving.
+
+`opts` is an options map of network configuration options that can include:
+```js
+  announce: true, // Announce the discovery key on the swarm
+  lookup: true  // Look up the discovery key on the swarm
+```
+
+### `networker.joined(discoveryKey)`
+Returns `true` if that discovery key is being swarmed.
+
+### `networker.flushed(discoveryKey)`
+Returns true if the swarm has discovered and attempted to connect to all peers announcing `discoveryKey`.
+
+#### `await networker.leave(discoveryKey)`
+Stop announcing or looking up the discovery key topic.
+
+Waits for the key to be fully unannounced before resolving.
+
+#### `await networker.close()`
+Shut down the swarm networker.
+
+This will close all replication streams and then destroy the swarm instance. It will wait for all topics to be unannounced, so it might take some time.
+
+### License
+MIT
---
+++ test/all.js
@@ -0,0 +1,233 @@
+const test = require('tape')
+const ram = require('random-access-memory')
+const dht = require('@dwebswarm/dht')
+const hypercoreCrypto = require('ddatabase-crypto')
+const HypercoreProtocol = require('ddatabase-protocol')
+const DWebstore = require('dwebstore')
+
+const SwarmNetworker = require('..')
+
+const BOOTSTRAP_PORT = 3100
+var bootstrap = null
+
+test('simple replication', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+
+  const core1 = store1.get()
+  const core2 = store2.get(core1.key)
+
+  await networker1.join(core1.discoveryKey)
+  await networker2.join(core2.discoveryKey)
+
+  await append(core1, 'hello')
+  const data = await get(core2, 0)
+  t.same(data, Buffer.from('hello'))
+
+  await cleanup([networker1, networker2])
+  t.end()
+})
+
+test('replicate multiple top-level cores', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+
+  const core1 = store1.get()
+  const core2 = store1.get()
+  const core3 = store2.get(core1.key)
+  const core4 = store2.get(core2.key)
+
+  await networker1.join(core1.discoveryKey)
+  await networker1.join(core2.discoveryKey)
+  await networker2.join(core2.discoveryKey)
+  await networker2.join(core3.discoveryKey)
+
+  await append(core1, 'hello')
+  await append(core2, 'world')
+  const d1 = await get(core3, 0)
+  const d2 = await get(core4, 0)
+  t.same(d1, Buffer.from('hello'))
+  t.same(d2, Buffer.from('world'))
+
+  await cleanup([networker1, networker2])
+  t.end()
+})
+
+test('replicate to multiple receivers', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+  const { store: store3, networker: networker3 } = await create()
+
+  const core1 = store1.get()
+  const core2 = store2.get(core1.key)
+  const core3 = store3.get(core1.key)
+
+  await networker1.join(core1.discoveryKey)
+  await networker2.join(core2.discoveryKey)
+  await networker3.join(core3.discoveryKey)
+
+  await append(core1, 'hello')
+  const d1 = await get(core2, 0)
+  const d2 = await get(core3, 0)
+  t.same(d1, Buffer.from('hello'))
+  t.same(d2, Buffer.from('hello'))
+
+  await cleanup([networker1, networker2, networker3])
+  t.end()
+})
+
+test('replicate sub-cores', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+
+  const core1 = store1.get()
+  const core3 = store2.get(core1.key)
+
+  await networker1.join(core1.discoveryKey)
+  await networker2.join(core3.discoveryKey)
+
+  const core2 = store1.get({ parents: [core1.key] })
+  const core4 = store2.get({ key: core2.key, parents: [core3.key]})
+
+  await append(core1, 'hello')
+  await append(core2, 'world')
+  const d1 = await get(core3, 0)
+  const d2 = await get(core4, 0)
+  t.same(d1, Buffer.from('hello'))
+  t.same(d2, Buffer.from('world'))
+
+  await cleanup([networker1, networker2])
+  t.end()
+})
+
+test('can replication with a custom keypair', async t => {
+  const keyPair1 = HypercoreProtocol.keyPair()
+  const keyPair2 = HypercoreProtocol.keyPair()
+  const { store: store1, networker: networker1 } = await create({ keyPair: keyPair1 })
+  const { store: store2, networker: networker2 } = await create({ keyPair: keyPair2 })
+
+  const core1 = store1.get()
+  const core3 = store2.get(core1.key)
+
+  await networker1.join(core1.discoveryKey)
+  await networker2.join(core3.discoveryKey)
+
+  const core2 = store1.get()
+  const core4 = store2.get({ key: core2.key })
+
+  await append(core1, 'hello')
+  await append(core2, 'world')
+  const d1 = await get(core3, 0)
+  const d2 = await get(core4, 0)
+  t.same(d1, Buffer.from('hello'))
+  t.same(d2, Buffer.from('world'))
+
+  t.same(networker1.streams[0].remotePublicKey, keyPair2.publicKey)
+  t.same(networker1.streams[0].publicKey, keyPair1.publicKey)
+  t.same(networker2.streams[0].remotePublicKey, keyPair1.publicKey)
+  t.same(networker2.streams[0].publicKey, keyPair2.publicKey)
+
+  await cleanup([networker1, networker2])
+  t.end()
+})
+
+test('join status only emits flushed after all handshakes', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+  const { store: store3, networker: networker3 } = await create()
+
+  const core1 = store1.get()
+  const core2 = store2.get(core1.key)
+  await append(core1, 'hello')
+
+  let join2Flushed = 0
+  let join3Flushed = 0
+  let join2FlushPeers = 0
+  let join3FlushPeers = 0
+
+  // If ifAvail were not blocked, the get would immediately return with null (unless the connection's established immediately).
+  await networker1.join(core1.discoveryKey)
+  networker2.on('flushed', dkey => {
+    if (!dkey.equals(core1.discoveryKey)) return
+    join2Flushed++
+    join2FlushPeers = core2.peers.length
+  })
+  await networker2.join(core1.discoveryKey)
+
+  const core3 = store3.get(core1.key)
+  networker3.on('flushed', (dkey) => {
+    if (!dkey.equals(core1.discoveryKey)) return
+    join3Flushed++
+    join3FlushPeers = core3.peers.length
+    allFlushed()
+  })
+  networker3.join(core1.discoveryKey)
+
+  async function allFlushed () {
+    t.same(join2Flushed, 1)
+    t.true(join2FlushPeers >= 1)
+    t.same(join3Flushed, 1)
+    t.true(join3FlushPeers >= 2)
+    await cleanup([networker1, networker2, networker3])
+    t.end()
+  }
+})
+
+test('can destroy multiple times', async t => {
+  const { networker } = await create()
+
+  await networker.close()
+  await networker.close()
+  t.pass('closed successfully')
+
+  await cleanup([networker])
+  t.end()
+})
+
+test.skip('each dwebstore only opens one connection per peer', async t => {
+  t.end()
+})
+
+async function create (opts = {}) {
+  if (!bootstrap) {
+    bootstrap = dht({
+      bootstrap: false
+    })
+    bootstrap.listen(BOOTSTRAP_PORT)
+    await new Promise(resolve => {
+      return bootstrap.once('listening', resolve)
+    })
+  }
+  const store =  new DWebstore(ram)
+  await store.ready()
+  const networker = new SwarmNetworker(store,  { ...opts, bootstrap: `localhost:${BOOTSTRAP_PORT}` })
+  return { store, networker }
+}
+
+function append (core, data) {
+  return new Promise((resolve, reject) => {
+    core.append(data, err => {
+      if (err) return reject(err)
+      return resolve()
+    })
+  })
+}
+
+function get (core, idx, opts = {}) {
+  return new Promise((resolve, reject) => {
+    core.get(idx, opts, (err, data) => {
+      if (err) return reject(err)
+      return resolve(data)
+    })
+  })
+}
+
+async function cleanup (networkers) {
+  for (let networker of networkers) {
+    await networker.close()
+  }
+  if (bootstrap) {
+    await bootstrap.destroy()
+    bootstrap = null
+  }
+}
